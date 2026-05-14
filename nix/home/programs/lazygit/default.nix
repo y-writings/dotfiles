66f31fbc3c1bd7dfe07350d4646045c8ebfaab01
Yoshiro M <96107941+y-writings@users.noreply.github.com>
2026-05-14 12:07:44 +0900

    Add lazygit ctrl+t command to retime selected commit

--- nix/home/programs/lazygit/default.nix
+++ nix/home/programs/lazygit/default.nix
@@ -14,6 +14,23 @@
           }
         ];
       };
+      customCommands = [
+        {
+          key = "<c-t>";
+          context = "commits";
+          description = "選択中コミットの日時を現在日時に更新";
+          prompts = [
+            {
+              type = "confirm";
+              title = "Commit日時更新";
+              body = "{{.SelectedLocalCommit.Sha}} の日時を現在日時に変更しますか？";
+            }
+          ];
+          command = "TARGET_SHA={{.SelectedLocalCommit.Sha}}; NOW=$(date -u '+%Y-%m-%dT%H:%M:%SZ'); git filter-branch -f --env-filter 'if [ \"$GIT_COMMIT\" = \"'$TARGET_SHA'\" ]; then export GIT_AUTHOR_DATE=\"'$NOW'\"; export GIT_COMMITTER_DATE=\"'$NOW'\"; fi' -- --all";
+          output = "terminal";
+          loadingText = "コミット日時を更新中...";
+        }
+      ];
     };
   };
 }
