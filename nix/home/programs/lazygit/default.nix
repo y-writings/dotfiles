574be04a1c9f8609c1d42928ee7a0e8452f02d37
Yoshiro M <96107941+y-writings@users.noreply.github.com>
2026-05-14 23:48:30 +0900

    Scope lazygit retime command to current branch

--- nix/home/programs/lazygit/default.nix
+++ nix/home/programs/lazygit/default.nix
@@ -26,7 +26,7 @@
               body = "{{.SelectedLocalCommit.Sha}} の日時を現在日時に変更しますか？";
             }
           ];
-          command = "TARGET_SHA={{.SelectedLocalCommit.Sha}}; NOW=$(date -u '+%Y-%m-%dT%H:%M:%SZ'); git filter-branch -f --env-filter 'if [ \"$GIT_COMMIT\" = \"'$TARGET_SHA'\" ]; then export GIT_AUTHOR_DATE=\"'$NOW'\"; export GIT_COMMITTER_DATE=\"'$NOW'\"; fi' -- --all";
+          command = "TARGET_SHA={{.SelectedLocalCommit.Sha}}; NOW=$(date -u '+%Y-%m-%dT%H:%M:%SZ'); BRANCH=$(git branch --show-current); git filter-branch -f --env-filter 'if [ \"$GIT_COMMIT\" = \"'$TARGET_SHA'\" ]; then export GIT_AUTHOR_DATE=\"'$NOW'\"; export GIT_COMMITTER_DATE=\"'$NOW'\"; fi' -- \"$BRANCH\"";
           output = "terminal";
           loadingText = "コミット日時を更新中...";
         }
